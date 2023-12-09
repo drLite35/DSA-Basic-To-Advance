90522e12bfaf67ccadc4c980eda3c9a3bed63ec7
Anurag Singh <principalquantum30@gmail.com>
2023-12-09 23:19:14 +0530

    C++ STL : Modify C++ STL codes.

--- 1-Cpp-concepts/19-Cpp-STL.cpp
+++ 1-Cpp-concepts/19-Cpp-STL.cpp
@@ -12,14 +12,14 @@
 using namespace std;
  
 //pairs
-// void pairs(){
-//     pair<int , int> p = {1,2};
-//     cout<<p.first<<" "<<p.second<<endl;
-//     pair<int , pair<int , int> > q = {1 , {3,4}};
-//     cout<<q.second.second<<endl;
-//     pair<int , int> arr[] ={{1,2},{2,3} , {5,6} };
-//     cout<<arr[2].first; 
-// }
+void pairs(){
+    pair<int , int> p = {1,2};
+    cout<<p.first<<" "<<p.second<<endl;
+    pair<int , pair<int , int> > q = {1 , {3,4}};
+    cout<<q.second.second<<endl;
+    pair<int , int> arr[] ={{1,2},{2,3} , {5,6} };
+    cout<<arr[2].first; 
+}
 
 //vectors
 void vectors(){
